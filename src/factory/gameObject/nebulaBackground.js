942a192ccab977447f4378ccc77e0dce85b67c39
David-Vandensteen <dvandensteen@gmail.com>
2023-05-21 15:13:29 +0200

    Fix : nebula background keyName

--- src/factory/gameObject/nebulaBackground.js
+++ src/factory/gameObject/nebulaBackground.js
@@ -2,14 +2,14 @@ import { GameObject } from '#src/model/gameObject';
 
 export default class NebulaBackgroundFactoryGameObject extends GameObject {
   load() {
-    this.kaboom.loadSprite('nebula', 'sprites/nebula.jpg');
+    this.kaboom.loadSprite(this.keyName, 'sprites/nebula.jpg');
     return this;
   }
 
   add() {
     this.kaboom.add([
       this.kaboom.pos(0, 0),
-      this.kaboom.sprite('nebula'),
+      this.kaboom.sprite(this.keyName),
     ]);
 
     return this;
